a7ce90226ec9d9ccf1336da14fc4998828e10e92
Tanmay Kushwaha <151605969+Tanmaykushwaha01@users.noreply.github.com>
2024-03-15 10:15:52 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat js code as newer version
+
+// alert( 3+3) not possible because we are using nodejs not browser
+
+
+//              DATA TYPES
+// Number
+// BigInt
+// String
+// Boolean
+// null => stand alone value (representation og empty value)
+// undefined
+// Symbol => finding uniqueness
+
+// Object
+
+
+console.log(typeof null); // object 
+console.log(typeof undefined); // undefined
