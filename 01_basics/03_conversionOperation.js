24d3790c6bddb5d39bdb6919926a0988cdda7096
Tanmay Kushwaha <151605969+Tanmaykushwaha01@users.noreply.github.com>
2024-03-16 07:02:52 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,34 +1,73 @@
-// age = "21"
+// // age = "21"
+// // console.log(typeof age);
+
+// let age = 20
 // console.log(typeof age);
 
-let age = 20
-console.log(typeof age);
-
-age = "21abc"
-let valueInNumber = Number(age)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// age = "21abc"
+// let valueInNumber = Number(age)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
-// "33" => 33
-// "33abs" => NaN   => js dont have strict check thats why people use typescript    
-// true = 1; false = 0
+// // "33" => 33
+// // "33abs" => NaN   => js dont have strict check thats why people use typescript    
+// // true = 1; false = 0
 
-let isLoggedIn = 1;
-let isLoggedIntoBoolean = Boolean(isLoggedIn);
-console.log(typeof isLoggedIntoBoolean);
-console.log(isLoggedIntoBoolean);
-// 1 or greater number => true; 0 => false; 'name' => true; "" => false;
+// let isLoggedIn = 1;
+// let isLoggedIntoBoolean = Boolean(isLoggedIn);
+// console.log(typeof isLoggedIntoBoolean);
+// console.log(isLoggedIntoBoolean);
+// // 1 or greater number => true; 0 => false; 'name' => true; "" => false;
 
-let someNumber = 21
-let stringNumber = Number(someNumber);
-console.log(typeof stringNumber);
-console.log(stringNumber);
-//21 => "21";
+// let someNumber = 21
+// let stringNumber = Number(someNumber);
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
+// //21 => "21";
 
-let nullVal =null;
-let nullIntoNumber = Number(nullVal);
-console.log(typeof nullIntoNumber);
-console.log(nullIntoNumber);
-// null => 0  like if weather app didnt send null data due to issue then it will show 0. be aware
+// let nullVal = null;
+// let nullIntoNumber = Number(nullVal);
+// console.log(typeof nullIntoNumber);
+// console.log(nullIntoNumber);
+// // null => 0  like if weather app didnt send null data due to issue then it will show 0. be aware
+
+
+
+// ------------------------------------------- Operations -------------------------------------------
+
+let val = 21;
+let negativeVal = -val;
+console.log(negativeVal);
+
+// console.log( 6 + 3);
+// console.log( 6 - 3);
+// console.log( 6 * 3);
+// console.log( 6 / 3);
+// console.log( 6 ** 3);
+// console.log( 6 % 3);
+
+// let str1 = 'hello'
+// let str2 = ' Tanmay'
+// let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);  // left to right // string // 12
+// console.log(1 + "2");  //Number // 12
+// console.log("1" + 2 + 2); // string  // 122
+// console.log(1 + 2 + "2"); //Number  //32
+
+
+//                          prefix and postfix
+// let x = 3;
+// const y = x++;
+
+// console.log(`x:${x}, y:${y}`);
+// // output: "x:4, y:3"
+
+// let a = 3;
+// const b = ++a;
+
+// console.log(`a:${a}, b:${b}`);
+// // output: "x:4, y:4" 
 
