5495caca941a0ec03985623b3e4ffcb5bf48cd68
Tanmay Kushwaha <151605969+Tanmaykushwaha01@users.noreply.github.com>
2024-03-16 09:07:47 +0000

    Strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,37 @@
+// let myName = "tanmay";
+// let repoCount = 5;
+// // console.log(name + repoCount );
+// console.log(`my name is ${myName} and i had created ${repoCount} repo on github`);
+//  // template lateral and do string interpolation
+
+ 
+const fullName = new String('Tanmay Kushwaha')
+// console.log(fullName[0]);
+// console.log(fullName.valueOf());
+// console.log(fullName.length);
+// console.log(fullName.toLowerCase());
+// console.log(fullName.toUpperCase());
+// console.log(fullName.charAt(4));
+// console.log(fullName.indexOf('a'));
+
+// const newName = fullName.substring(0, 6);
+// console.log(newName);
+
+// const gameName = fullName.slice(0, 7);
+// console.log(gameName);
+
+// const trimName = '       tanmay     '
+// console.log(trimName.trim());
+// console.log(trimName);
+
+// const str1 = "tanmay"
+// const str2 = " kushwaha"
+// console.log(str1.concat('',str2));
+
+// const url = "https://tanmaykushwaha.com/tanmay%20kushwaha";
+// console.log(url.includes('tanmay')); // true
+// console.log(url.split("%20")); // [ 'https://tanmaykushwaha.com/tanmay', 'kushwaha' ]
+
+// const newUrl = url.replace("%20", "-"); // https://tanmaykushwaha.com/tanmay-kushwaha
+// console.log(newUrl);
+
