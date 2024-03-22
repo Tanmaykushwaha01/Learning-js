865988596768181828fcdd6b95959b6a93799460
Tanmaykushwaha01 <tanmaykushwaha0101@gmail.com>
2024-03-23 02:12:45 +0530

    for each loop

---
+++ 05_iterations/for_each.js
@@ -0,0 +1,43 @@
+const coding = ["js", "ruby", "java", "python", "cpp"];
+
+// coding.forEach( function (val) {
+//     console.log(val);
+// })
+
+// coding.forEach( (item) => {
+//     console.log(item);
+// })
+
+function printCoding(values) {
+  console.log(values);
+}
+// coding.forEach(printCoding); // dont need to add function execution -> () only give reference -> printCoding
+
+
+// coding.forEach( (item, index, arr) => {
+//     console.log(item, index, arr);
+// })  // output => js 0 [ 'js', 'ruby', 'java', 'python', 'cpp' ]
+// ruby 1 [ 'js', 'ruby', 'java', 'python', 'cpp' ]
+// java 2 [ 'js', 'ruby', 'java', 'python', 'cpp' ]
+// python 3 [ 'js', 'ruby', 'java', 'python', 'cpp' ]
+// cpp 4 [ 'js', 'ruby', 'java', 'python', 'cpp' ]
+
+
+const myCoding = [
+  {
+    languageName: "javascript",
+    languageFileName: "js",
+  },
+  {
+    languageName: "java",
+    languageFileName: "java",
+  },
+  {
+    languageName: "python",
+    languageFileName: "py",
+  },
+];
+
+myCoding.forEach( (item) => {
+    console.log(`currently i am learning ${item.languageName}`);
+})
